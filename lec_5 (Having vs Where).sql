8ef38abd59347e430aab610f26ac5e78b2e98ed7
NavneetSingh-18 <navneetsingh1882004@gmail.com>
2025-07-07 21:35:36 +0530

    Add files via upload

---
+++ lec_5 (Having vs Where).sql
@@ -0,0 +1,69 @@
+-- Having vs Where
+
+-- Both were created to filter rows of data, but they filter 2 separate things
+-- Where is going to filters rows based off columns of data
+-- Having is going to filter rows based off aggregated columns when grouped
+
+SELECT gender, AVG(age)
+FROM employee_demographics
+GROUP BY gender
+;
+
+
+-- let's try to filter on the avg age using where
+
+SELECT gender, AVG(age)
+FROM employee_demographics
+WHERE AVG(age) > 40
+GROUP BY gender
+;
+-- this doesn't work because of order of operations. On the backend Where comes before the group by. So you can't filter on data that hasn't been grouped yet
+-- this is why Having was created
+  
+  
+# Now Lets See which code will work
+SELECT gender, AVG(age)
+FROM employee_demographics
+HAVING AVG(age) > 40
+GROUP BY gender;
+
+-- the HAVING clause must come after the GROUP BY — and MySQL expects GROUP BY before HAVING.
+  
+  
+SELECT gender, AVG(age) as AVG_age
+FROM employee_demographics
+GROUP BY gender
+HAVING AVG_age > 40
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
